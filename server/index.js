81ee844458f67cf5f278abe21024b504ee8f5536
David <merehatibebadane@gmail.com>
2021-12-03 05:40:31 -0800

    ready to deploy

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import errorhandler from 'errorhandler'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
-// import path from 'path'
+import path from 'path'
 // import engine from 'consolidate'
 import expressSession from 'express-session';
 import {apiRouter} from '../api/api.js';
@@ -17,13 +17,7 @@ app.use(expressSession({
     saveUninitialized: false
 }));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  
+
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
@@ -35,11 +29,14 @@ app.use(errorhandler());
 app.use(morgan('dev'));
 
 //app.set('view engine', 'html')
-app.use(express.static('public'))
+let __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
 });
 
 // module.exports = app;
-
